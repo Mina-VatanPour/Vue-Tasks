2e10655c20e0f7968cbdd60fe7a149afb04d701d
Mina Vatanpour <mv.manaiger1996@gmail.com>
2023-06-17 13:22:58 +0330

    Receive data and display in the table

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,14 +1,34 @@
 <template>
   <div class="row">
     <div class="input-group mb-5">
-      <input type="search" class="form-control" placeholder="search..." aria-label="Username"
+      <input type="search" v-model="searchInput" class="form-control" value="" placeholder="search..."
+             aria-label="Username"
              aria-describedby="basic-addon1">
-      <span class="input-group-text" id="basic-addon1">search</span>
+      <button class="input-group-text" id="basic-addon1" @click="searchTitle">search</button>
+
+<!--      <template v-if="">-->
+<!--        <TableView :todos-list=""/>-->
+<!--      </template>-->
+
     </div>
   </div>
 </template>
 
 <script setup>
+
+import {fetchData} from "@/mixin/fetchData";
+import {ref} from "vue";
+// import TableView from "@/components/TableView";
+
+const searchInput = ref('');
+
+const searchTitle = () => {
+  const todosList = fetchData('https://jsonplaceholder.typicode.com/todos');
+  if (todosList) {
+    const foundTitle = todosList.filter(todo => todo.title.includes(searchInput.value) || todo.title === searchInput.value)
+    console.log(foundTitle)
+  }
+}
 
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,21 @@
 
   <div class="row home container">
 
-<!--    <SearchBox/>-->
     <TableView :todos-list="todosList" :users="users"/>
-<div>
 
-</div>
   </div>
 </template>
 
 
 <script setup>
+
 import TableView from '@/components/TableView';
-// import SearchBox from '@/components/SearchBox';
 import {fetchData} from '@/mixin/fetchData.js';
 
 const todosList = fetchData('https://jsonplaceholder.typicode.com/todos');
 const users = fetchData('https://jsonplaceholder.typicode.com/users');
+
+
 </script>
 
 
