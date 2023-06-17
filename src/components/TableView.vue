5eab8590be195fcdcb8454f2dcf5842ac69d78ee
Mina Vatanpour <mv.manaiger1996@gmail.com>
2023-06-17 17:21:31 +0330

    use computed() to display users names

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -1,24 +1,22 @@
 <template>
   <div class="row">
     <table class="table" id="tableTodos">
-
       <thead class="table-dark">
       <tr>
         <th>Id</th>
-        <th>UserId -> UserName</th>
+        <th>UserName</th>
         <th>Title</th>
         <th>Status</th>
       </tr>
       </thead>
-
       <tbody class="table-light" v-for="todo in props.todosList" :key="todo.id">
       <tr>
         <td>{{ todo.id }}</td>
-        <td>{{ usersById[todo.userId] ? usersById[todo.userId].name : '' }}</td>
+        <td>{{ getUsersNamesById.names[todo.userId] ? getUsersNamesById.names[todo.userId].name : '' }}</td>
         <td>{{ todo.title }}</td>
         <td>{{ todo.completed ? todo.completed = 'completed' : '' }}</td>
-      </tr>
 
+      </tr>
       </tbody>
     </table>
 
@@ -27,19 +25,24 @@
 </template>
 
 <script setup>
-
-import {defineProps} from 'vue';
+import {computed, defineProps, reactive} from 'vue';
 
 const props = defineProps({
   todosList: Object,
   users: Object
 })
 
-let usersById = [];
-for (let user of props.users) {
-  usersById[user.id] = user;
+const getUsersNamesById = reactive({
+  names : computed(()=>{
+      let usersById = [];
+      for (let user of props.users) {
+        usersById[user.id] = user;
+      }
+      return usersById;
+  })
+})
 
-}
+
 </script>
 
 <style scoped>
